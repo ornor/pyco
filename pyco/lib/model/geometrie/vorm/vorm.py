fdd6169af97f2853fc83de3862fb646250e1f97c
Jaap van Veldhoven <jaapv@nveldhoven.nl>
2021-10-31 10:43:09 +0100

    update

--- pyco/lib/model/geometrie/vorm/vorm.py
+++ pyco/lib/model/geometrie/vorm/vorm.py
@@ -5,6 +5,100 @@ import matplotlib.pyplot as plt
 
 from pyco.model import BasisObject, Waarde, Vector, Lijn, Knoop
 
+class VormFuncties:
+    """Statische hulpfuncties voor uitrekenen eigenschappen vorm."""
+
+    @classmethod
+    def punt_aan_linker_zijde(cls, la, lb, p):
+        """Checkt of punt p aan linker kant van lijn la-lb ligt."""
+        return (((lb[0] - la[0]) * (p[1] - la[1])
+                 - (lb[1] - la[1]) * (p[0] - la[0])) > 0)
+
+    @classmethod
+    def punt_aan_rechter_zijde(cls, la, lb, p):
+        """Checkt of punt p aan rechter kant van lijn la-lb ligt."""
+        return (((lb[0] - la[0]) * (p[1] - la[1])
+                 - (lb[1] - la[1]) * (p[0] - la[0])) < 0)
+
+    @classmethod
+    def punt_op_lijn(cls, la, lb, p):
+        """Checkt of punt p op lijn la-lb ligt, tussen la en lb."""
+        op_lijn = (((lb[0] - la[0]) * (p[1] - la[1])
+                    - (lb[1] - la[1]) * (p[0] - la[0])) == 0)
+        if not op_lijn:
+            return False
+        if la[0] == lb[0]:     # verticale lijn
+            return ((p[1] <= lb[1] and p[1] >= la[1]) or
+                    (p[1] >= lb[1] and p[1] <= la[1]))
+        else:
+            return ((p[0] <= lb[0] and p[0] >= la[0]) or
+                    (p[0] >= lb[0] and p[0] <= la[0]))
+
+    @classmethod
+    def lijn_raakt_lijn(cls, l1a, l1b, l2a, l2b):
+        """Checkt of lijn l1a-l1b de lijn l2a-l2b kruist of raakt, op de lijnstukken TUSSEN de punten en INCLUSIEF de punten zelf."""
+        if (cls.punt_op_lijn(l1a, l1b, l2a) or
+                cls.punt_op_lijn(l1a, l1b, l2b) or
+                cls.punt_op_lijn(l2a, l2b, l1a) or
+                cls.punt_op_lijn(l2a, l2b, l1b)):
+            # uiteinde lijn ligt op andere lijn
+            return True
+
+        l1xmin = min(l1a[0], l1b[0])
+        l1xmax = max(l1a[0], l1b[0])
+        l2xmin = min(l2a[0], l2b[0])
+        l2xmax = max(l2a[0], l2b[0])
+        l1ymin = min(l1a[1], l1b[1])
+        l1ymax = max(l1a[1], l1b[1])
+        l2ymin = min(l2a[1], l2b[1])
+        l2ymax = max(l2a[1], l2b[1])
+        if (l1xmin > l2xmax or l2xmin > l1xmax or
+                l1ymin > l2ymax or l2ymin > l1ymax):
+            # lijn ligt helemaal links/rechts/onder/boven andere lijn
+            return False
+
+        # l1 = a * x + b
+        # l2 = c * x + d
+        if l1b[0] - l1a[0] == 0:        # l1 is een verticale lijn
+            x = l1a[0]
+            if l2b[0] - l2a[0] == 0: 	# l2 is ook verticaal
+                return False
+            else:
+                c = 1.0 * (l2b[1] - l2a[1]) / (l2b[0] - l2a[0])
+                d = l2a[1] - l2a[0] * c
+                y = c * x + d
+                if ((y >= l1a[1] and y <= l1b[1]) or
+                         (y >= l1b[1] and y <= l1a[1])):
+                    return True
+                else:
+                    return False
+        elif l2b[0] - l2a[0] == 0:          # l2 is een verticale lijn
+            x = l2a[0]
+            if l1b[0] - l1a[0] == 0:	    # l1 is ook verticaal
+                return False
+            else:
+                a = 1.0 * (l1b[1] - l1a[1]) / (l1b[0] - l1a[0])
+                b = l1a[1] - l1a[0] * a
+                y = a * x + b
+                if ((y >= l2a[1] and y <= l2b[1]) or
+                         (y >= l2b[1] and y <= l2a[1])):
+                    return True
+                else:
+                    return False
+        else:                           # beide lijnen zijn diagonaal
+            a = 1.0 * (l1b[1] - l1a[1]) / (l1b[0] - l1a[0])
+            b = l1a[1] - l1a[0] * a
+            c = 1.0 * (l2b[1] - l2a[1]) / (l2b[0] - l2a[0])
+            d = l2a[1] - l2a[0] * c
+            # l1 gelijk aan l2
+            if a - c == 0:              # parallel
+                return False
+            x = 1.0 * (d - b) / (a - c)
+        if x >= l1xmin and x >= l2xmin and x <= l1xmax and x <= l2xmax:
+            return True
+        else:
+            return False
+
 
 class Vorm(BasisObject):
     """Betreft een meetkundig 2D vorm met bijbehorende eigenschappen.
@@ -14,6 +108,8 @@ class Vorm(BasisObject):
 
 
     """
+
+    fn = VormFuncties()
 
     def __init__(self,
                  lijn:Lijn,
@@ -82,6 +178,22 @@ class Vorm(BasisObject):
         plt.axis('equal')
         plt.show()
 
+    def __getitem__(self, index):
+        """Retourneert subset Numpy array object met getallen (zonder eenheid)."""
+        return self.array[index]
+
+    def __len__(self):
+        """Geeft aantal knopen."""
+        # laatste knoop is zelfde als eerste knoop
+        return len(self.array) - 1
+
+    def __iter__(self):
+        """Itereert over knopen en geeft Knoop objecten terug."""
+        for k_array in self.array[:-1]:
+            k = Knoop(k_array.tolist())
+            k.eenheid = self.eenheid
+            yield k
+
     @property
     def A(self) -> float:
         """Berekent oppervlakte en retourneert een float object."""
@@ -133,12 +245,3 @@ class Vorm(BasisObject):
     def O_(self) -> Waarde:
         """Berekent omtrek en retourneert een Waarde object."""
         return Waarde(self.O, self.eenheid)
-
-    def __getitem__(self, index):
-        """Retourneert subset Numpy array object met getallen (zonder eenheid)."""
-        return self.array[index]
-
-    def __len__(self):
-        """Geeft aantal knopen."""
-        # laatste knoop is zelfde als eerste knoop
-        return len(self.array) - 1
--- test/model/geometrie/vorm/test_vorm.py
+++ test/model/geometrie/vorm/test_vorm.py
@@ -11,6 +11,52 @@ Vorm = doc.model.Vorm
 
 class TestVorm(unittest.TestCase):
 
+    fn = Vorm([(0,0), (1,1)]).fn
+
+    def test_fn_punt_aan_linker_zijde(self):
+        assert self.fn.punt_aan_linker_zijde((3,4),(5,6), (4,10))
+        assert self.fn.punt_aan_linker_zijde((0,0),(0,5), (-4,10))
+        assert self.fn.punt_aan_linker_zijde((0,0),(5,0), (2,2))
+
+    def test_fn_punt_aan_rechter_zijde(self):
+        assert self.fn.punt_aan_rechter_zijde((3,4),(5,6), (4,1))
+        assert self.fn.punt_aan_rechter_zijde((0,0),(0,5), (4,10))
+        assert self.fn.punt_aan_rechter_zijde((0,0),(5,0), (2,-2))
+
+    def test_fn_punt_op_lijn(self):
+        assert self.fn.punt_op_lijn((3,4),(5,6), (4,5))
+        assert not self.fn.punt_op_lijn((3,4),(5,6), (1,2))
+        assert not self.fn.punt_op_lijn((3,4),(5,6), (6,7))
+        assert self.fn.punt_op_lijn((0,0),(0,5), (0,2))
+        assert not self.fn.punt_op_lijn((0,0),(0,5), (0,-2))
+        assert not self.fn.punt_op_lijn((0,0),(0,5), (0,10))
+        assert self.fn.punt_op_lijn((0,0),(5,0), (2,0))
+        assert not self.fn.punt_op_lijn((0,0),(5,0), (-2,0))
+        assert not self.fn.punt_op_lijn((0,0),(5,0), (10,0))
+
+    def test_fn_lijn_raakt_lijn(self):
+        # kuisen diagonale lijnen
+        assert self.fn.lijn_raakt_lijn((0,0),(10,10), (0,10),(10,0))
+        assert self.fn.lijn_raakt_lijn((10,10),(0,0), (0,10),(10,0))
+        assert self.fn.lijn_raakt_lijn((0,0),(10,10), (10,0),(0,10))
+        assert self.fn.lijn_raakt_lijn((10,10),(0,0), (10,0),(0,10))
+        # uiteinde op andere lijn
+        assert self.fn.lijn_raakt_lijn((5,5),(0,0), (10,0),(0,10))
+        assert self.fn.lijn_raakt_lijn((10,10),(5,5), (10,0),(0,10))
+        assert self.fn.lijn_raakt_lijn((10,10),(0,0), (5,5),(0,10))
+        assert self.fn.lijn_raakt_lijn((10,10),(0,0), (10,0),(5,5))
+        # l1 en/of l2 is horizontaal/verticaal
+        assert self.fn.lijn_raakt_lijn((0,5),(10,5), (0,10),(10,0))
+        assert self.fn.lijn_raakt_lijn((5,0),(5,10), (0,10),(10,0))
+        assert self.fn.lijn_raakt_lijn((0,0),(10,10), (0,5),(10,5))
+        assert self.fn.lijn_raakt_lijn((0,0),(10,10), (5,0),(5,10))
+        assert self.fn.lijn_raakt_lijn((0,5),(10,5), (5,0),(5,10))
+        # een aantal negatieve checks
+        assert not self.fn.lijn_raakt_lijn((0,0),(4,4), (0,10),(10,0))
+        assert not self.fn.lijn_raakt_lijn((0,0),(4,4), (10,0),(0,-0.0001))
+        assert not self.fn.lijn_raakt_lijn((0,0),(0,2), (0,3),(0,5))
+        assert not self.fn.lijn_raakt_lijn((0,0),(2,0), (3,0),(5,0))
+
     def test_Vorm(self):
         self.assertRaises(TypeError, lambda: Lijn('str', 'str'))
         assert Lijn(Knoop(1, 2), Knoop(3, 4)) == Lijn([Knoop(1, 2), Knoop(3, 4)])
@@ -48,8 +94,6 @@ print('A =', repr(v2.A_))
 print('z =', repr(v2.z_))
 print('O =', repr(v2.O_))
 print()
-
-
 
 
 if __name__ == '__main__':
